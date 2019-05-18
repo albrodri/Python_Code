00a154640363e707a374968f1a1e8ddadc746006
Alberto J Rodriguez <RodriguezAlberto24@gmail.com>
2019-05-18 11:09:21 -0400

    working to update the script to allow for headers

--- JSON file to Pandas/JSON to Pandas.py
+++ JSON file to Pandas/JSON to Pandas.py
@@ -1,27 +1,49 @@
 # -*- coding: utf-8 -*-
 """
     purpose: To get the results from an API call and convert to a pandas data frame
-    author: alberto rodriguez
-    date: 2018+12+26
+    author: Alberto Rodriguez
+    date: 2019+05+18
 """
 
-import requests
+import json
+import urllib.request
 import pandas as pd
 
 
-def callApi():
+def call_api():
+    """
+    To make the GET API call
 
-    url = 'https://jsonplaceholder.typicode.com/comments' # should be 500
-    headers = {
-        'Content-Type': 'application/json'
+    :return: a list that contains the response from the HTTP GET call
+    """
+
+    # url = 'https://jsonplaceholder.typicode.com/comments'
+    url = 'https://jsonplaceholder.typicode.com/posts?'
+    hdr = {
+        'Content-Type': 'application/json',
+        'userId': '1',
     }
 
-    returned_results = requests.get(url, headers=headers)
+    # Create the base HTTP request
+    response = urllib.request.Request(url)
 
-    return returned_results.content
+    # TODO: Add a parameter to filter the data for a specific value
+    # Add the headers to the HTTP request
+    for header, value in hdr.items():
+        response.add_header(header, value)
+
+    # Make the HTTP request to retrieve the JSON
+    response = urllib.request.urlopen(response)
+
+    # Read the response into a list
+    data = json.loads(response.read())
+
+    return data
 
 
 if __name__ == '__main__':
 
-    json_results = pd.read_json(callApi(), orient='columns')
-    json_results.to_csv('testing.csv', encoding='utf-8', index=False)
+    responseDf = pd.DataFrame(call_api())
+    # json_results.to_csv('testing.csv', encoding='utf-8', index=False)
+
+    # print(responseDf)
